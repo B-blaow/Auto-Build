6e086362c4961930cdb7e269345f69850765947b
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-01-03 19:00:49 +0800

    Update openwrt2.sh

--- openwrt2.sh
+++ openwrt2.sh
@@ -2,4 +2,10 @@
 #===============================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' $(pwd)/package/base-files/files/bin/config_generate
-
+#
+#
+#
+#
+#加入nano
+sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
+grep -q '^CONFIG_PACKAGE_nano=y' .config || echo 'CONFIG_PACKAGE_nano=y' >> .config
