ef59b7b090fe2650e61b077458454de1726f6f62
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-01-03 18:55:18 +0800

    Update immortalwrt2.sh

--- immortalwrt2.sh
+++ immortalwrt2.sh
@@ -3,4 +3,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' $(pwd)/package/base-files/files/bin/config_generate
 
-
+#
+#
+#
+#
+#
+#在.config加入nano
+sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
+grep -q '^CONFIG_PACKAGE_nano=y' .config || echo 'CONFIG_PACKAGE_nano=y' >> .config
