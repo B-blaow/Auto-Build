e9ba55d476bba912e091f9bc749b1c2ff900d970
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-01-03 18:58:47 +0800

    Update lede2.sh

--- lede2.sh
+++ lede2.sh
@@ -14,32 +14,6 @@ sed -i 's/192.168.1.1/192.168.5.5/g' $(pwd)/package/base-files/files/bin/config_
 
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
-# ==== 强制启用的插件 ====
-SAFE_PKGS=(
-  luci-app-ttyd
-  mosdns
-  luci-app-mosdns
-  luci-i18n-mosdns-zh-cn
-  luci-app-homeproxy
-  luci-i18n-homeproxy-zh-cn
-  luci-i18n-adguardhome-zh-cn
-  luci-app-adguardhome
-  nikki
-  luci-app-nikki
-  luci-i18n-nikki-zh-cn
-  ddns-scripts-cloudflare
-  cloudflared
-  luci-app-cloudflared
-  wireguard-tools
-  SING_BOX_BUILD_WIREGUARD
-  kmod-wireguard
-  luci-app-mwan3
-  mwan3
-  luci-i18n-mwan3-zh-cn
-)
-
-for pkg in "${SAFE_PKGS[@]}"; do
-  sed -i "s/^# CONFIG_PACKAGE_${pkg} is not set/CONFIG_PACKAGE_${pkg}=y/" .config
-  grep -q "CONFIG_PACKAGE_${pkg}=y" .config || \
-    echo "CONFIG_PACKAGE_${pkg}=y" >> .config
-done
+# ==== 加入nano ====
+sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
+grep -q '^CONFIG_PACKAGE_nano=y' .config || echo 'CONFIG_PACKAGE_nano=y' >> .config
