d6365bd2abb03d23d00620de69dd425df471ad3e
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-01-05 20:21:12 +0800

    Update lede2.sh

--- lede/lede2.sh
+++ lede/lede2.sh
@@ -3,16 +3,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.5/g' $(pwd)/package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-
-#2. Custom settings
-#sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
-#sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
-#sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 # ==== 加入nano ====
 sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
